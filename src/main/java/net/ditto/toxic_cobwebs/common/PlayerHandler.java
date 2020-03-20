51984a8c611e9399d0993363c456255bc626b6da
DittoOfDarkness <58920765+DittoOfDarkness@users.noreply.github.com>
2020-03-19 17:06:40 -0700

    Add files via upload

--- src/main/java/net/ditto/toxic_cobwebs/common/PlayerHandler.java
+++ src/main/java/net/ditto/toxic_cobwebs/common/PlayerHandler.java
@@ -1,10 +1,14 @@
 package net.ditto.toxic_cobwebs.common;
 
+import net.ditto.toxic_cobwebs.toxic_cobwebs;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockWeb;
+import net.minecraft.entity.Entity;
 import net.minecraft.init.Blocks;
 import net.minecraft.potion.Potion;
 import net.minecraft.potion.PotionEffect;
+import net.minecraft.util.math.AxisAlignedBB;
+import net.minecraft.util.math.BlockPos;
 import net.minecraftforge.event.entity.living.LivingEvent.LivingUpdateEvent;
 import net.minecraftforge.event.world.BlockEvent.HarvestDropsEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
@@ -26,8 +30,12 @@ public class PlayerHandler
 	@SubscribeEvent
 	public void onUpdate(LivingUpdateEvent event)
 	{
-		Block block = event.getEntity().getEntityWorld().getBlockState(event.getEntity().getPosition()).getBlock();
-		if (block instanceof BlockWeb)
+		Entity entity = event.getEntity();
+		Block block1 = entity.getEntityWorld().getBlockState(new BlockPos(entity.getPosition())).getBlock();
+		
+		Block block2 = entity.getEntityWorld().getBlockState(new BlockPos(entity.posX, entity.posY + entity.height, entity.posZ)).getBlock();
+		
+		if (block1 instanceof BlockWeb || block2 instanceof BlockWeb)
 		{
 			if(!event.getEntityLiving().isPotionActive(Potion.getPotionById(19)))
 			{
