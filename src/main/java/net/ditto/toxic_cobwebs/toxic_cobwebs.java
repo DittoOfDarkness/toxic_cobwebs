e2d54a7d07362117cd2f20cc2413cc3cae4a430f
DittoOfDarkness <58920765+DittoOfDarkness@users.noreply.github.com>
2020-03-19 17:06:17 -0700

    Add files via upload

--- src/main/java/net/ditto/toxic_cobwebs/toxic_cobwebs.java
+++ src/main/java/net/ditto/toxic_cobwebs/toxic_cobwebs.java
@@ -4,21 +4,20 @@ import java.util.ArrayList;
 import java.util.List;
 
 import net.ditto.toxic_cobwebs.common.PlayerHandler;
+import net.minecraftforge.client.event.RenderGameOverlayEvent;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.Mod.Instance;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 @Mod(modid = toxic_cobwebs.MOD_ID, name = toxic_cobwebs.MOD_NAME, version = toxic_cobwebs.VERSION)
 public class toxic_cobwebs {
 	
 	public static final String MOD_ID = "toxic_cobwebs";
 	public static final String MOD_NAME = "Toxic Cobwebs";
-	public static final String VERSION = "1.0.0";
-	
-	@Instance
-	public static toxic_cobwebs instance;
+	public static final String VERSION = "1.0.1";
 	
 	@EventHandler
 	public void init(FMLInitializationEvent event)
